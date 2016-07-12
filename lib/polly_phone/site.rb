6903b6acb521d945262a96d88568894fe588eb15
Artyom Kalmykov <artyom.kalm@gmail.com>
2016-07-12 22:14:37 +0300

    Add some description for params

--- lib/polly_phone/site.rb
+++ lib/polly_phone/site.rb
@@ -13,6 +13,23 @@ module PollyPhone
 
     DEFAULT_DESCRIPTION_TYPE = "Description"
     
+    # Create object with configuration for parsing with following params
+    # @name - title of web-site we want to parse
+    # @main_url - main url of web-site we want to parse
+    # @brands_url - additional path to catalog page where we want to get rand names
+    # @brand_link_path - path for Nokogiri search method to find element with href param for brand
+    # @brand_name_item - path for Nokogiri at_xpath method to find element with name of brand
+    #                    (only use when brand name is within another element, otherwise get name of brand link element)
+    # @brand_source - only use when we need another element param to find brand content, f.e. 'alt'
+    #                 (otherwise use element content) 
+    # @model_link_path - path for Nokogiri search method to find element with href param for model
+    # @model_image_item - path for Nokogiri at_xpath method to find element with src param for model image
+    # @model_name_item - path for Nokogiri at_xpath method to find element with content for model name
+    # @spec_path - path for Nokogiri at_xpath method to find container with phone description
+    # @spec_type_item - path for Nokogiri at_xpath method to find description type within the container 
+    # @spec_category_item - path for Nokogiri at method to find description category within the container
+    # @spec_body_item - path for Nokogiri at method to find description text within the container
+    # @search_page - additional path for main url for web-site search
     def initialize(file_path)
       site_config = symbolize_keys(YAML.load_file(file_path))
       assign_attributes(site_config)
